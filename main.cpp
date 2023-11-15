dd57c6a4eb87ac50317a2b2da3356adfd835d278
kamil <kamil@user.com>
2023-11-15 15:55:59 +0100

    typos

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 
 void usage()
 {
-        std::cout << "wordCounter:" << std::endl;
-        std::cout << "usage:" << std::endl;
-        std::cout << "  wordCounter [FILE PATH] option" << std::endl;
-        std::cout << "where option are:" << std::endl;
-        std::cout << "-?, -h, --help                  display usage information" << std::endl;
-        std::cout << "-t, --threads                   set how many threads will be used " << std::endl;
-        std::cout << "-q, --quiet                     disable printing to standard output " << std::endl;
+    std::cout << "wordCounter:" << std::endl;
+    std::cout << "usage:" << std::endl;
+    std::cout << "  wordCounter [FILE PATH] option" << std::endl;
+    std::cout << "where option are:" << std::endl;
+    std::cout << "-?, -h, --help                  display usage information" << std::endl;
+    std::cout << "-t, --threads                   set how many threads will be used " << std::endl;
+    std::cout << "-q, --quiet                     disable printing to standard output " << std::endl;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
@@ -27,7 +27,7 @@ int main(int argc, char* argv[])
         return 0;
     }
     std::string filePath(argv[1]);
-    if(not std::filesystem::exists(filePath))
+    if (not std::filesystem::exists(filePath))
     {
         std::cout << "file :" << filePath << " does not exist" << std::endl;
         return -1;
@@ -38,8 +38,12 @@ int main(int argc, char* argv[])
     {
         std::string options(argv[i]);
         if (options == "-t" || options == "--threads")
-            if (i + 1 >= argc)
+        {
+            if (i + 1 <= argc)
+            {
                 threadsAmount = atoi(argv[i + 1]);
+            }
+        }
         if (options == "-?" || options == "-h" || options == "--help")
             usage();
         if (options == "-q" || options == "--quiet")
@@ -47,10 +51,9 @@ int main(int argc, char* argv[])
     }
     if (threadsAmount == 1)
     {
-    std::cout << "WARNING: hardware_concurrency recommend using 1 thread use -t to set how many thread program should use" << std::endl;
+        std::cout << "WARNING: hardware_concurrency recommend using 1 thread use -t to set how many thread program should use" << std::endl;
     }
-    
-    Counter c(filePath,threadsAmount);
+    Counter c(filePath, threadsAmount);
 
     auto amount = c.countWords();
     if (not quiet)
@@ -58,6 +61,5 @@ int main(int argc, char* argv[])
         std::cout << amount << std::endl;
     }
 
-    return amount;    
+    return amount;
 }
-
